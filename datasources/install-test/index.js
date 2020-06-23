c1a6e38781e7aa9b2fedc0d5ac5a72ce3d12c6ab
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-06-23 18:04:00 +0200

    Add/Update Component:   datasource.github install-test

--- datasources/install-test/index.js
+++ datasources/install-test/index.js
@@ -1,12 +1,20 @@
 const {Octokit} = require("@octokit/rest");
 const _ = require("lodash");
+const joke = require('give-me-a-joke');
 
 module.exports = async function(owner, repo, token=undefined) {
-    var x = [4, 8, 15, 16, 23, 42];
-    var y = [42, 23, 16, 15, 8, 4];
-    y = _.sortBy(y, [function(o) {return o}])
-    return Promise.resolve({
-        x: x,
-        y: y
+    return new Promise(async (resolve, reject) => {
+        var x = [4, 8, 15, 16, 23, 42];
+        var y = [42, 23, 16, 15, 8, 4];
+        y = _.sortBy(y, [function(o) {return o}])
+        joke.getRandomCNJoke(j => {
+            resolve({
+                x: x,
+                y: y,
+                joke: j
+            });
+        }, err => {
+            reject(err);
+        });
     });
 }
