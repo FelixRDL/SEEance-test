437980897fa6f1df2b203309faa71a4f4ac9ca39
FelixRDL <felix1.riedl@stud.uni-regensburg.de>
2020-06-29 10:06:36 +0200

    Delete test.js

--- datasources/commits-list/test.js
+++
@@ -1,9 +0,0 @@
-/**
- * Simply execute npm test -- <GITHUB_PROJECT_URL> <TOKEN>
- */
-const script = require('./index')
-script(process.argv[2], process.argv[3]).then(result => {
-    console.log(result);
-}, error => {
-    console.error(error);
-})
